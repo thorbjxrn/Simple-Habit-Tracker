e0cbfb3dfdf355bdd6c0b2d06f2cce81451906d9
thorbjørn <tbonvik@gmail.com>
2024-07-31 21:30:47 +0200

    Initial Commit

---
+++ simpleHabitTracker/simpleHabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleHabitTrackerApp.swift
+//  simpleHabitTracker
+//
+//  Created by Thorbjørn on 31/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct simpleHabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
